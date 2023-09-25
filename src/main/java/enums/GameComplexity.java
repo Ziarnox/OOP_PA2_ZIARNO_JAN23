91fce872187bcc0d0d37f31d889ad1a99b4eaa4c
Ziarnox <norbertziarno@gmail.com>
2023-09-25 10:10:40 +0200

    Stworzenie enum potrzebnych do projektu

---
+++ src/main/java/enums/GameComplexity.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum GameComplexity {
+    EASY,
+    MEDIUM,
+    HARD
+}
---
+++ src/main/java/enums/GameType.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum GameType {
+    PUZZLE,
+    STORYTELLING,
+    CARDGAME
+}
