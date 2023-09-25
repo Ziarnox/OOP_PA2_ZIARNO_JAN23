077c8d370c4e5b658934662689e942a5bcd2c872
Ziarnox <norbertziarno@gmail.com>
2023-09-25 10:58:24 +0200

    skonczenie klasy boardGame, dodanie geterow i seterow oraz funkcji

--- src/main/java/models/boardGame.java
+++ src/main/java/models/boardGame.java
@@ -1,4 +1,5 @@
 package models;
+import java.util.List;
 
 import enums.gameComplexity;
 import enums.gameType;
@@ -10,11 +11,59 @@ public class boardGame {
     private int maxPlayers;
     private int playingTime;
     private gameComplexity complexity;
+    private boolean singlePlayer;
+    private boolean free;
+    private boolean storytelling;
+    private boolean onlinePlay;
+    private boolean onsitePlay;
+    private String location;
+    private boolean active;
+
+
+    public boardGame(String name, gameType type, int minPlayers, int maxPlayers, int playingTime, gameComplexity complexity, boolean onlinePlay, boolean onsitePlay, String location) {
+        this.name = name;
+        this.type = type;
+        this.minPlayers = minPlayers;
+        this.maxPlayers = maxPlayers;
+        this.playingTime = playingTime;
+        this.complexity = complexity;
+        this.onlinePlay = onlinePlay;
+        this.onsitePlay = onsitePlay;
+        this.location = location;
+        this.active = false;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    public boolean isStorytellingGameActive() {
+        return this.type == gameType.STORYTELLING && this.active;
+    }
+
+    public boolean canStartGame(List<player> players) {
+        if (free && players.size() >= minPlayers && players.size() <= maxPlayers) {
+            if (singlePlayer && players.size() == 1) {
+                return true;
+            } else if (!storytelling && !isStorytellingGameActive()) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public String toString() {
+        String onlinePlayStatus = onlinePlay ? "Yes" : "No";
+        String onsitePlayStatus = onsitePlay ? "Yes" : "No";
         return "Name: " + name + "\nType: " + type +
                 "\nMin Players: " + minPlayers + "\nMax Players: " + maxPlayers +
                 "\nPlaying Time: " + playingTime + " minutes" + "\nComplexity: " + complexity +
-                "\nOnline Play: Yes\nOnsite Play: Yes";
+                "\nOnline Play: " + onlinePlayStatus + "\nOnsite Play: " + onsitePlayStatus +
+                (onsitePlay ? "\nLocation: " + location : "") +
+                "\nActive: " + active;
     }
 }
+
